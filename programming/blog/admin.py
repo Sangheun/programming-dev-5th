ec2df3ad4a2e6fa28a979370d7060a2bc1e3ae78
Lee Sangheun <heun0108@gmail.com>
2016-07-14 12:21:04 +0900

    queryset 실습

--- programming/blog/admin.py
+++ programming/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Post, Blog, Author, Entry
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Blog)
+admin.site.register(Author)
+admin.site.register(Entry)
--- programming/blog/models.py
+++ programming/blog/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+import re
 
-# Create your models here.
+from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
+
+def lnglat_validator(lnglat):
+    if not re.match(r'^(\d+\.?\d*),(\d+\.?\d*)$', lnglat):
+        raise forms.ValidationError('Invalid LngLat Type')
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name = '제목')
+    content = models.TextField(help_text='Markdown 문법을 써 주세요.')
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, validators=[lnglat_validator], help_text='경도, 위도 포맷으로 입력')
+    created_at = models.DateTimeField(default=timezone.now)
+    test_field = models.IntegerField(default=10
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Entry(models.Model):
+
+    blog = models.ForeignKey(Blog)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.headline
+
